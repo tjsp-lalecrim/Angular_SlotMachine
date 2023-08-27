2502d63f303bdbd5d7df3229c24b0a5d47974f95
LEANDRO MORAES DE ALECRIM <lalecrim@tjsp.jus.br>
2023-08-27 18:08:59 -0300

    first template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Angular Games';
+  @ViewChild('gridList', { static: true }) gridList?: ElementRef;
+  gridColumns = 3;
+
+  games: any[] = [];
+
+  constructor() {
+    this.setGames();
+    this.setGridColumns();
+  }
+
+  setGames() {
+    this.games = [
+      {
+        title: 'Game #1',
+        category: 'Classic',
+        description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+      },
+      {
+        title: 'Game #2',
+        category: 'Puzzle',
+        description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Vestibulum eget iaculis est. In vitae mauris lectus.`,
+      },
+      {
+        title: 'Game #3',
+        category: 'Card',
+        description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Vestibulum eget iaculis est. In vitae mauris lectus. 
+        Suspendisse vitae fermentum eros, quis sollicitudin erat. 
+        Fusce leo erat, tincidunt et scelerisque ac, facilisis at risus.`,
+      },
+      {
+        title: 'Game #4',
+        category: 'Card',
+        description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Vestibulum eget iaculis est. In vitae mauris lectus. 
+        Suspendisse vitae fermentum eros, quis sollicitudin erat.`,
+      },
+      {
+        title: 'Game #5',
+        category: 'Classic',
+        description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+      },
+      {
+        title: 'Game #6',
+        category: 'Action',
+        description: `
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Vestibulum eget iaculis est. In vitae mauris lectus.`,
+      },
+    ];
+  }
+
+  setGridColumns() {
+    const gridWidth =
+      this.gridList?.nativeElement.clientWidth ?? window.innerWidth;
+    if (gridWidth > 1200) this.gridColumns = 3;
+    else if (gridWidth > 800) this.gridColumns = 2;
+    else this.gridColumns = 1;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatGridListModule,
+    MatToolbarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
