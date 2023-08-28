b3563f0b42382e82ed073856ee00e68969e50b07
lalecrim@tjsp.jus.br <lalecrim@tjsp.jus.br>
2023-08-28 16:53:32 -0300

    include MatButtonsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { SlotMachineComponent } from './components/slot-machine/slot-machine.component';
-import { HomeComponent } from './pages/home/home.component';
+
 import { GameComponent } from './pages/game/game.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SlotMachineComponent } from './components/slot-machine/slot-machine.component';
 
 @NgModule({
   declarations: [AppComponent, SlotMachineComponent, HomeComponent, GameComponent],
@@ -17,6 +19,7 @@ import { GameComponent } from './pages/game/game.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatButtonModule,
     MatCardModule,
     MatGridListModule,
     MatToolbarModule,
@@ -24,4 +27,4 @@ import { GameComponent } from './pages/game/game.component';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
